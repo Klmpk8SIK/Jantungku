0a9c8382bfd8ef322fe5fef9c7c560e10562ce9d
Klmpk8SIK <klmpk8sik@gmail.com>
2025-03-13 14:47:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const anggotaButton = document.getElementById("showAnggota");
     const anggotaSection = document.getElementById("anggotaSection");
+    const logoKelompok = document.getElementById("logoKelompok");
 
     anggotaButton.addEventListener("click", function () {
         if (anggotaSection.style.display === "none" || anggotaSection.style.display === "") {
             anggotaSection.style.display = "block";
+            logoKelompok.style.display = "block";
             anggotaButton.textContent = "Sembunyikan Anggota Kelompok";
         } else {
             anggotaSection.style.display = "none";
+            logoKelompok.style.display = "none";
             anggotaButton.textContent = "Tampilkan Anggota Kelompok";
         }
     });
